876a24debd03cd11cde00f21f29f360d88bdf6ff
Shay <sgaming24@outlook.com>
2021-12-13 23:16:58 +0000

    Check for alphanumeric address

--- index.js
+++ index.js
@@ -48,7 +48,25 @@ formatNumber = (num) => {
     };
 };
 
+isAlphaNumeric = (str) => {
+    var code, i, len;
+  
+    for (i = 0, len = str.length; i < len; i++) {
+      code = str.charCodeAt(i);
+      if (!(code > 47 && code < 58) && // numeric (0-9)
+          !(code > 64 && code < 91) && // upper alpha (A-Z)
+          !(code > 96 && code < 123)) { // lower alpha (a-z)
+        return false;
+      }
+    }
+    return true;
+};
+
 fetchData = async (address="83iyvgajvmCSMLeZsEpQUbP3LwxU1zxsGVaTJ2G8n7CBVHQvsHeEYzDKzjXrYjc2HfA6FxLS6Rx2aYZ6WiEzpAHvNHBgGst") => {
+
+    if (!isAlphaNumeric(address)) {
+        address = "83iyvgajvmCSMLeZsEpQUbP3LwxU1zxsGVaTJ2G8n7CBVHQvsHeEYzDKzjXrYjc2HfA6FxLS6Rx2aYZ6WiEzpAHvNHBgGst"
+    };
 
     const opts = {
         headers: {
